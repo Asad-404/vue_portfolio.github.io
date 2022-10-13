057fa2ab0c003449951c8fa2dabc0b279cf65667
unknown <asad.hnk@gmail.com>
2022-10-13 14:18:28 +0600

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import homeComponent from "../components/home/homeComponent.vue";
+</script>
+
+<template>
+  <main>
+    <h1 class="green">You did it!</h1>
+    <homeComponent />
+  </main>
+</template>
